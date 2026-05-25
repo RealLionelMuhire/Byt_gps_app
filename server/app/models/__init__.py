74eaac9d95b2e3f6ee4e83c22b0da559004d1570
Lionel Muhire <muhirelionel@gmail.com>
2026-05-25 22:58:52 +0200

    Export Vehicle and Subscription models

--- server/app/models/__init__.py
+++ server/app/models/__init__.py
@@ -6,5 +6,7 @@ from app.models.user import User
 from app.models.geofence import Geofence
 from app.models.trip import Trip
 from app.models.trip_settings import TripSettings
+from app.models.vehicle import Vehicle
+from app.models.subscription import Subscription, Payment
 
-__all__ = ['Device', 'Location', 'User', 'Geofence', 'Trip', 'TripSettings']
+__all__ = ['Device', 'Location', 'User', 'Geofence', 'Trip', 'TripSettings', 'Vehicle', 'Subscription', 'Payment']
